02cab13909408ad802391ecc4956655f0d810012
Netan Mangal <51133680+netanmangal@users.noreply.github.com>
2021-10-23 19:47:05 +0530

    formatting and removing comments

--- ICO.sol
+++ ICO.sol
@@ -27,7 +27,6 @@ library SafeMath {
   }
 }
 
-
 contract ERC20Interface {
   function totalSupply() public view returns (uint);
   function balanceOf(address tokenOwner) public view returns (uint balance);
@@ -39,10 +38,6 @@ contract ERC20Interface {
   event Transfer(address indexed from, address indexed to, uint tokens);
   event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
-
-
-
-
 
 contract ICO is ERC20Interface {
     
@@ -72,7 +67,7 @@ contract ICO is ERC20Interface {
         icoEnds = now + 4 weeks;
         bonusEnds = now + 1 weeks;
         totalContributors = 1;
-        allTokens = 100;
+        allTokens = 1000000;
         isICORunning = true;
         admin = msg.sender;
         balances[admin] = allTokens;
@@ -81,15 +76,10 @@ contract ICO is ERC20Interface {
     function buyTokens() public payable {
         require(isICORunning);
         uint tokens = msg.value;
-        if (balances[msg.sender] == 0) totalContributors++;
+        if (balances[msg.sender] == 0 && msg.value > 0) totalContributors++;
         balances[msg.sender] += tokens;
         allTokens += tokens;
     }
-    
-    
-    
-    // --------------------------------------------------------------------------------------------------------
-    
     
     function totalSupply() public view returns (uint256) {
         return allTokens.sub(balances[address(0)]);
@@ -106,31 +96,12 @@ contract ICO is ERC20Interface {
         return true;
     }
 
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for `spender` to transferFrom(...) `tokens`
-    // from the token owner's account
-    //
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-    // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces
-    // ------------------------------------------------------------------------
     function approve(address spender, uint tokens) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
 
-
-    // ------------------------------------------------------------------------
-    // Transfer `tokens` from the `from` account to the `to` account
-    //
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the `from` account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
     function transferFrom(address from, address to, uint tokens) public returns (bool success) {
         balances[from] = balances[from].sub(tokens);
         allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
@@ -139,19 +110,9 @@ contract ICO is ERC20Interface {
         return true;
     }
 
-
-    // ------------------------------------------------------------------------
-    // Returns the amount of tokens approved by the owner that can be
-    // transferred to the spender's account
-    // ------------------------------------------------------------------------
     function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
-    
-    
-    // --------------------------------------------------------------------------------------------------------
-    
-    
     
     function getAdminAdress() view public returns(address) {
         return admin;
